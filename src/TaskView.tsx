5836b0bd3bfdaf3d939ba782d3838a00551b5952
VanJackal <v@njackal.com>
2023-05-30 12:40:32 -0500

    DueDate component WIP

--- src/TaskView.tsx
+++ src/TaskView.tsx
@@ -1,6 +1,40 @@
 import React from "react";
 import {Task,getTask,createTask,updateTask} from "./api";
 
+let DueDate = ({value, onChange}:{value:Date|null,onChange:(value:Date)=>void}) => {// todo finish this component
+    let date:Date|null = value? new Date(value.getDate()) : null;
+    let hour:number = value?.getMinutes() || 0;
+    let minute:number = value?.getHours() || 0;
+
+    const sendChange = () => {
+
+    }
+    return (
+        <div className="DueDate">
+            <input type="date" value={date?.toISOString()}/>
+            <select id="hour" value={hour}>
+                {(() => {
+                    let options = []
+                    for (let i = 0; i <= 23; i++) {
+                        options.push(<option value={i}>{i.toString().padStart(2,"0")}</option>)
+                    }
+                    return options
+                })()}
+            </select>
+            :
+            <select id="minute" value={minute}>
+                {(() => {
+                    let options = []
+                    for (let i = 0; i <= 55; i+=5) {
+                        options.push(<option value={i}>{i.toString().padStart(2,"0")}</option>)
+                    }
+                    return options
+                })()}
+            </select>
+        </div>
+    )
+}
+
 let TaskView = ({taskId,select,refresh}:{taskId:string,select:Function,refresh:Function}) => {
     let [taskData, setTask] = React.useState<Task>({
         _id: "", state: 0, title: ""
@@ -25,7 +59,7 @@ let TaskView = ({taskId,select,refresh}:{taskId:string,select:Function,refresh:F
         <>
             <input placeholder="Task Name" value={taskData.title} onChange={ event => setTask({...taskData,title:event.target.value})}/>
             <textarea placeholder="Description" value={taskData.description} onChange={event => setTask({...taskData,description:event.target.value})}/>
-            <input type="datetime-local" value={taskData.dueDate?new Date(taskData.dueDate).toLocaleString():""} onChange={event => setTask({...taskData,dueDate:new Date(event.target.value).toISOString()})}/>
+            <DueDate value={taskData.dueDate?new Date(taskData.dueDate):null} onChange={value => setTask({...taskData,dueDate:value.toISOString()})}/>
             <input type="text" placeholder="Tags" value={taskData.tags} onChange={event => setTask({...taskData,tags:event.target.value.split(",")})}/>
             <label>Alert </label>
             <input type="checkbox"/>
